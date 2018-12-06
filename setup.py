459801389b8321dbb751ad1c06af2cb79e4aaea8
wgroeneveld <wouter.groeneveld@gmail.com>
2018-12-06 08:26:51 +0100

    long description update

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='opbtest',
       version='0.1',
       description='Open PicoBlaze Assembler Test package',
+      long_description='See https://github.com/wgroeneveld/opbtest/blob/master/README.md',
       url='https://github.com/wgroeneveld/opbtest',
       author='Wouter Groeneveld',
       author_email='wouter.groeneveld@kuleuven.be',
